9ee2b30c3415f497384b6d91fdfae5c07e51673e
Idan Gazit <idan@gazit.me>
2018-06-13 12:57:04 +0300

    It's in Israel!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello, basecamp!'))
+app.get('/', (req, res) => res.send('Hello, basecamp Israel!'))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
